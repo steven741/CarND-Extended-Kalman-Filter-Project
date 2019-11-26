0c42147e99774919b8ab9472b16987666cb26f21
Steven Watson <16228203+steven741@users.noreply.github.com>
2019-11-26 08:09:03 +0800

    cleaned up code base Made things cleaner and, easier to read.

--- lib/EKF.hs
+++ lib/EKF.hs
@@ -59,17 +59,16 @@ predict t kf =
     -- Delta-time. Essentially a timestep.
     dt =
       (fromIntegral (t - filterTime)) / 1000000.0
+    dt2 = dt ** 2
+    dt3 = dt ** 3
+    dt4 = dt ** 4
 
-    -- Acceleration noise tuning perameters
+    -- Acceleration noise tuning parameters
     ax = 3
     ay = 3
 
     noiseAx = ax ** 2
     noiseAy = ay ** 2
-
-    dt2 = dt ** 2
-    dt3 = dt ** 3
-    dt4 = dt ** 4
 
     -- State transition matrix
     f = (4><4) [1, 0, dt,  0,
@@ -79,13 +78,13 @@ predict t kf =
 
     -- Noise covariance matrix
     q = (4><4) [-- Row 1
-                0.25 * noiseAx * dt4, 0, 0.5 * noiseAx * dt3, 0,
+                noiseAx * 0.25 * dt4, 0, noiseAx * 0.5 * dt3, 0,
                 -- Row 2
-                0, 0.25 * noiseAy * dt4, 0, 0.5 * noiseAy * dt3,
+                0, noiseAy * 0.25 * dt4, 0, noiseAy * 0.5 * dt3,
                 --  Row 3
-                0.5 * noiseAx * dt3, 0, noiseAx * dt2, 0,
+                noiseAx *0.5 * dt3, 0, noiseAx * dt2, 0,
                 -- Row 4
-                0, 0.5 * noiseAy * dt3, 0, noiseAy * dt2]
+                0, noiseAy * 0.5 * dt3, 0, noiseAy * dt2]
 
     -- Noise vector
     v  = 4 |> [ax * 0.5 * dt2,
@@ -93,7 +92,7 @@ predict t kf =
                ax * dt,
                ay * dt]
 
-    -- Estimated step based on CV motion model
+    -- Estimated step based on constant velocity (CV) motion model
     x = f #> kf_x kf + v
     p = f <> kf_p kf <> (tr f) + q
 
@@ -113,6 +112,7 @@ update (Laser px py t) kf =
     y = 2 |> [px, py] - (h #> kf_x kf')
 
     -- Kalman Filter Equations
+    i  = ident 4
     ht = tr h
     s  = h <> kf_p kf' <> ht + r
     si = inv s
@@ -120,7 +120,7 @@ update (Laser px py t) kf =
 
     -- Estimation based on kalman filter gain
     x' = kf_x kf' + (k #> y)
-    p' = (ident 4 - k <> h) <> kf_p kf'
+    p' = (i - k <> h) <> kf_p kf'
 
 
 update (Radar rho phi rho' t) kf =
@@ -161,6 +161,7 @@ update (Radar rho phi rho' t) kf =
     y = 3 |> [z ! 0, atan2 (sin (z ! 1)) (cos (z ! 1)), z ! 2]
 
     -- Kalman filter equations
+    i  = ident 4
     ht = tr h
     s  = h <> kf_p kf' <> ht + r
     si = inv s
@@ -168,4 +169,4 @@ update (Radar rho phi rho' t) kf =
 
     -- Estimation based on kalman filter gain
     x' = kf_x kf' + (k #> y)
-    p' = (ident 4 - k <> h) <> kf_p kf'
+    p' = (i - k <> h) <> kf_p kf'
